4ce6e4423ad8df146cb7e538682f13bb9eca0126
Denise Fugihara <denise@retroca.com.br>
2016-09-13 15:48:30 -0300

    Fix documentation

--- app/controllers/spree/admin/pages_controller.rb
+++ app/controllers/spree/admin/pages_controller.rb
@@ -3,7 +3,7 @@ module Spree
 		class PagesController < ResourceController
 
 			def display
-				@page = Spree::Page.by_store(current_store).visible.find_by!(slug: '/'+params[:slug])
+				@page = Spree::Page.visible.find_by!(slug: '/'+params[:slug])
 				@current = params[:slug]
 			end
 
